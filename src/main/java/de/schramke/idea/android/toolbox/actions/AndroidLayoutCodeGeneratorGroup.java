5108f5e1629de68426271ec5d973bc45e922ea4e
Björn Schramke <bjoern@schramke-online.de>
2015-03-20 16:47:26 +0100

    some testing

---
+++ src/main/java/de/schramke/idea/android/toolbox/actions/AndroidLayoutCodeGeneratorGroup.java
@@ -0,0 +1,17 @@
+package de.schramke.idea.android.toolbox.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+
+/**
+ * Created by bschramke on 20.03.2015.
+ */
+public class AndroidLayoutCodeGeneratorGroup extends DefaultActionGroup{
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+    }
+
+
+}
